772ca02ae9e20ac5ae0e95452edb6345cef8e752
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-10-26 11:55:46 -0300

    Update Corrigindo

--- script-lista-distribuição-export.sh
+++ script-lista-distribuição-export.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 date > tempo_gerar_lista.txt
 ZMPROV="/opt/zimbra/bin/zmprov"
-PATCH= /mnt/backup/WEBMAIL/listas
+PATCH=/mnt/backup/WEBMAIL/listas
+
 mkdir -p $PATCH
 
-$ZMPROV gadl | grep > $PATCH/ld/distlist.tmp
+$ZMPROV gadl > $PATCH/distlist.tmp
 for lista in `cat $PATCH/distlist.tmp`; do
 
 $ZMPROV gdl $lista |grep zimbraMailForwa |cut -d" " -f2 &> $PATCH/$lista;
