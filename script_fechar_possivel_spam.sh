2f69230098a766da138231cfd2a5c176b7f0433d
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-11-24 14:52:57 -0300

    Filtrando Dados

--- script_fechar_possivel_spam.sh
+++ script_fechar_possivel_spam.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#jeferson Araujo de Sousa
+cat /var/log/mail.log| grep "sasl_method=PLAIN" | cut -d: -f5 | sort | uniq -c | sort -n > spam
 
-cat /var/log/mail.log| grep "sasl_method=PLAIN" | cut -d: -f5 | sort | uniq -c | sort -n
+  #pega quantidade por ip
+  QTD=$(cat spam | grep client | awk -F " " '{print $1}')
+  IP = $(cat spam |   awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   | cut -d "=" -f3)
+  CONTA = $(awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   | cut -d "=" -f6)
+
+  echo "$qtd|$ip|$conta" >> /tmp/temp
