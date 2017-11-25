508cf3a177104bffaa7844101e563d2018f50832
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-11-25 09:45:05 -0300

    Organizando e preparando dados para filtragem

--- script_fechar_possivel_spam.sh
+++ script_fechar_possivel_spam.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 #jeferson Araujo de Sousa
 max=$(wc -l spam | cut -d " " -f1)
-SPAM=$(cat spam)
+#spam=$(cat spam)
 #cat /var/log/mail.log| grep "sasl_method=PLAIN" | cut -d: -f5 | sort | uniq -c | sort -n > spam
   for i in `seq 1 $max` ; do
-    #pega quantidade por ip
-    tail -$i $SPAM | grep client | awk -F " " '{print $1}' &> /tmp/qtd
-    QTD=$(cat /tmp/qtd )
-    tail -$i $SPAM |   awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   | cut -d "=" -f3 &> /tmp/ip
-    IP=$(cat /tmp/ip)
-    tail -$i $SPAM |awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   | cut -d "=" -f6 &> /tmp/conta
-    CONTA=$(cat /tmp/conta)
-    echo "$QTD|$IP|$CONTA" &> /tmp/temp
+    tail -$i spam |  awk -F "sasl_username=" '{print $1"="$2}'| awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   |  awk -F " " '{print $0"="$1}' | cut -d "=" -f7,3,6 >> /tmp/temp
+
 
   done
