17b63d866396039e4e605114cb138c9f07033151
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-11-25 11:21:17 -0300

    Reordenando  o a manipulação de dados

--- script_fechar_possivel_spam.sh
+++ script_fechar_possivel_spam.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 #jeferson Araujo de Sousa
 max=$(wc -l spam | cut -d " " -f1)
-#spam=$(cat spam)
-#cat /var/log/mail.log| grep "sasl_method=PLAIN" | cut -d: -f5 | sort | uniq -c | sort -n > spam
-  for i in `seq 1 $max` ; do
-    tail -$i spam |  awk -F "sasl_username=" '{print $1"="$2}'| awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   |  awk -F " " '{print $0"="$1}' | cut -d "=" -f7,3,6 >> /tmp/temp
-
-
-  done
+cat /var/log/mail.log| grep "sasl_method=PLAIN" | cut -d: -f5 | sort | uniq -c | sort -n > spam
+cat spam |  awk -F "sasl_username=" '{print $1"="$2}'| awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   |  awk -F " " '{print $0"="$1}' | cut -d "=" -f7,3,6 >> /tmp/temp
