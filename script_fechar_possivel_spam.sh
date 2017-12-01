0e20e9b9e116b81feca06ad28c0caf1c2ae9c403
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-01 07:08:49 -0300

    Dropa ips

--- script_fechar_possivel_spam.sh
+++ script_fechar_possivel_spam.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #jeferson Araujo de Sousa
-max=$(wc -l spam | cut -d " " -f1)
-cat /var/log/mail.log| grep "sasl_method=PLAIN" | cut -d: -f5 | sort | uniq -c | sort -n > spam
-cat spam |  awk -F "sasl_username=" '{print $1"="$2}'| awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   |  awk -F " " '{print $0"="$1}' | cut -d "=" -f7,3,6 >> /tmp/temp
+#max=$(wc -l spam | cut -d " " -f1)
+#cat /var/log/mail.log| grep "sasl_method=PLAIN" | cut -d: -f5 | sort | uniq -c | sort -n > spam
+#cat spam |  awk -F "sasl_username=" '{print $1"="$2}'| awk -F "[" '{print $1"="$2}' | awk -F "]" '{print $1"="$2}'   |  awk -F " " '{print $0"="$1}' | cut -d "=" -f7,3,6 >> /tmp/temp
+#cat /tmp/temp  | cut -d "=" -f1 > blacklistcat
+
+for IP in $(cat blacklistcat); do
+
+  iptables -I INPUT -s $IP -j DROP
+
+done
