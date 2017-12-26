1647b38c44c94e0401deed3948abbc24cef7173b
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-26 12:16:03 -0300

    Update formato de datas

--- backup-contas-por-etapas-7dias.sh
+++ backup-contas-por-etapas-7dias.sh
@@ -4,7 +4,8 @@ ZMPROV="/opt/zimbra/bin/zmprov"
 ZMMAILBOX="/opt/zimbra/bin/zmmailbox -z -m "
 CAM="/mnt/backup"    #caminho para onde será exportado os dados
 HARI=`date --date='7 days ago' +"%m/%d/%Y"`
-date2=`date +%m/%d/%Y`
+date=`date --date='7 days ago' +"%d_%m_%Y"`
+date2=`date +%d_%m_%Y`
 query="&query=after:$HARI"
 date > tempo_obter-contas.txt
 max=100
@@ -16,7 +17,7 @@ for i in `seq 1 $max`; do
                 MAIL=$(cat bkp-contas.txt | head -n $i | tail -n 1)
 
                 #exportar os arquivos da conta
-                $ZMMAILBOX  $MAIL getRestURL "//?fmt=tgz" >   $CAM/$MAIL/$MAIL.$HARI-$date2.tgz
+                $ZMMAILBOX  $MAIL getRestURL "//?fmt=tgz" >   $CAM/$MAIL/$MAIL.$date-$date2.tgz
                 echo $MAIL >> exports-conta.log;
 
 done
