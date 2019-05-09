ed2cabd21597ebf292240f4c6783a0eefb76d220
Jeferson Araujo <jefersonaraujo95@gmail.com>
2019-05-09 10:15:22 -0300

    update

--- script-criar-conta.sh
+++ script-criar-conta.sh
@@ -2,21 +2,21 @@
 # Cria as contas no Zimbra
 ZMPROV="/opt/zimbra/bin/zmprov"
 ZMMAILBOX="/opt/zimbra/bin/zmmailbox -z -m "
-
+CAM="/mnt/backup"
 date > criar-contas.txt
-for DADOS in $(cat /caminho/contas/contas.txt); do
+for DADOS in $(cat $CAM/contas.txt); do
 
-  EMAIL=$(grep Conta /caminho/contas/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
-  DNAME=$(grep Display /caminho/contas/$DADOS/dados-$DADOS | awk -F " " '{print $2" 		"$3" "$4" "$5}')
-  PNAME=$(grep Nome /caminho/contas/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
-  SNAME=$(grep Sobrenome /caminho/contas/$DADOS/dados-$DADOS | awk -F " " '{print 	$2}')
-  SENHA=$(grep Senha /caminho/contas/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
-  QOUTA=$(grep Qouta /caminho/contas/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
+  EMAIL=$(grep Conta $CAM/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
+  DNAME=$(grep Display $CAM/$DADOS/dados-$DADOS | awk -F " " '{print $2" 		"$3" "$4" "$5}')
+  PNAME=$(grep Nome $CAM/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
+  SNAME=$(grep Sobrenome $CAM/$DADOS/dados-$DADOS | awk -F " " '{print 	$2}')
+  SENHA=$(grep Senha $CAM/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
+  QOUTA=$(grep Qouta $CAM/$DADOS/dados-$DADOS | awk -F " " '{print $2}')
 
 #Criar Conta primeiro e gera hash aleartoria
   $ZMPROV ca "$DADOS" password displayName "$DNAME" givenName "$PNAME" sn "$SNAME"
 
-#modifica a hash, pela a exportada do servidor de produção. 
+#modifica a hash, pela a exportada do servidor de produção.
   $ZMPROV ma  "$DADOS" userPassword "$SENHA" zimbraMailQuota "$QOUTA"
 
  done
